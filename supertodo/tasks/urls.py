40a63d14809ab14f30d3ebbf5bcea0313ca4d161
Moisés Pérez Bethencourt <moiicnfg@gmail.com>
2025-10-13 21:38:49 +0100

    Update_FirstSession_House

--- supertodo/tasks/urls.py
+++ supertodo/tasks/urls.py
@@ -5,7 +5,11 @@ from . import views
 app_name = 'tasks'
 urlpatterns = [
     path('', views.task_list, name='task-list'),
-    path('detail/', views.task_detail, name='task-detail'),
-    path('edit/', views.edit_task, name='edit-task'),
-    path('delete/', views.delete_task, name='delete-task'),
+    path('completed/', views.task_list_completed, name='task-list-completed'),
+    path('pending/', views.task_list_pending, name='task-list-pending'),
+    path('add/', views.add_task, name='add-task'),
+    path('<slug:task_slug>/', views.task_detail, name='task-detail'),
+    path('<slug:task_slug>/edit', views.edit_task, name='edit-task'),
+    path('<slug:task_slug>/delete', views.delete_task, name='delete-task'),
+    path('<slug:task_slug>/toggle', views.toogle_task, name='toogle-task'),
 ]
--- supertodo/tasks/views.py
+++ supertodo/tasks/views.py
@@ -1,4 +1,4 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
 
 from .models import Task
 
@@ -7,11 +7,25 @@ def task_list(request):
     tasks = Task.objects.all()
     return render(request, 'tasks/task/list.html', {'tasks': tasks})
 
+def task_list_completed(request):
+    tasks = Task.objects.filter(task_list_completed=True)
+    return render(request, 'tasks/task/list.html'), {'tasks': tasks}
 
-# def task_list_pending():
-# def task_list_completed():
-# def add_task():
-# def task_detail():
-# def delete_task():
-# def edit_task():
-# def toogle_task():
+def task_list_pending(request):
+    tasks = Task.objects.filter(task_list_completed=False)
+    return render(request, 'tasks/task/list.html'), {'tasks': tasks}
+
+def task_detail(request, slug):
+     tasks = Task.objects.filter(slug=slug)
+     return render(request, 'tasks/task/detail.html', {'tasks': tasks})
+
+def add_task(request):
+    pass
+def delete_task(request):
+    pass
+def edit_task(request):
+    pass
+def toogle_task(request):
+    Task.completed = not Task.completed
+    Task.save()
+    return redirect('list.html')
