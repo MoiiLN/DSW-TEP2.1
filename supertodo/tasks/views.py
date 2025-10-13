009bd3be8e518c22d9051d33d84214d9edb3b33c
Moisés Pérez Bethencourt <moiicnfg@gmail.com>
2025-10-13 22:10:10 +0100

    Update_FirstSession_House

--- supertodo/tasks/views.py
+++ supertodo/tasks/views.py
@@ -1,4 +1,4 @@
-from django.shortcuts import render, redirect
+from django.shortcuts import render
 
 from .models import Task
 
@@ -7,25 +7,33 @@ def task_list(request):
     tasks = Task.objects.all()
     return render(request, 'tasks/task/list.html', {'tasks': tasks})
 
+
 def task_list_completed(request):
-    tasks = Task.objects.filter(task_list_completed=True)
+    tasks = Task.objects.get(task_list_completed=True)
     return render(request, 'tasks/task/list.html'), {'tasks': tasks}
+
 
 def task_list_pending(request):
     tasks = Task.objects.filter(task_list_completed=False)
     return render(request, 'tasks/task/list.html'), {'tasks': tasks}
 
+
 def task_detail(request, slug):
-     tasks = Task.objects.filter(slug=slug)
-     return render(request, 'tasks/task/detail.html', {'tasks': tasks})
+    tasks = Task.objects.filter(slug=slug)
+    return render(request, 'tasks/task/detail.html', {'tasks': tasks})
+
 
 def add_task(request):
     pass
+
+
 def delete_task(request):
     pass
+
+
 def edit_task(request):
     pass
+
+
 def toogle_task(request):
-    Task.completed = not Task.completed
-    Task.save()
-    return redirect('list.html')
+    pass
